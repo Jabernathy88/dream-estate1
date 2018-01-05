e5c4a9b487534665d0b81d03e34d44d15857c439
Jabernathy88 <j.e.abernathy@gmail.com>
2018-01-05 15:45:55 -0500

    Finished delete route (without data). Will add data to all pages next.

--- routes/users.js
+++ routes/users.js
@@ -6,24 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('users/index', {})
 })
 
-// 2. POST, Create User
+// POST, Create User
 router.get('/new', (request, response) => {
   response.render('users/new', {/*pageTitle: 'New User'*/})
-})
+}) // need more steps
 
-// PUT
+// PUT, Edit User
+router.get('/edit', (request, response) => {
+  response.render('users/edit', {/*pageTitle: 'New User'*/})
+}) // need more steps
 
 // DELETE
+router.get('/delete', (request, response) => { // add :wild back in
+//  const userId = request.params.userId
 
+//  User.findByIdAndRemove(userId)
+//    .then(() => {
+      response.redirect('/users')
+//    })
+//    .catch((error) => {
+//      console.log(error)
+//    })
+})
 
 // 3. Show Single User
 router.get('/show', (request, response) => {
   response.render('users/show', {})
 })
-
-
-// 4. 
-
-// 5. 
 
 module.exports = router
