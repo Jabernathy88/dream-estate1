cdb3d2388b1b4a153a925ed2eee0923eaeaaf3d5
Jabernathy88 <j.e.abernathy@gmail.com>
2018-01-05 11:55:36 -0500

    Got users/index to work. Added basic css, the most basic

--- app.js
+++ app.js
@@ -1,52 +1,21 @@
 require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI); 
+const express = require('express')
+const mongoose = require('mongoose')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// Creating new Express app
+const app = express()
 
-var app = express();
+app.set('view engine', 'hbs')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(express.static('public'))
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 // Mongo connection set-up
 mongoose.Promise = global.Promise
@@ -66,6 +35,12 @@ mongoose.connection.on('error', (error) => {
   process.exit(-1)
 })
 
+// Registering controllers
+const users = require('./routes/users')
+app.use('/users', users)
+
+// my other controllers go here
+
 // Automatically redirect to the Users page on load
 app.get('/', (request, response) => {
   response.redirect('/users')
@@ -76,4 +51,3 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}`)
 })
-
