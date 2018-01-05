01dab466b6aecb384067c1c0f0d9c9f40ef2b55b
Jabernathy88 <j.e.abernathy@gmail.com>
2018-01-05 13:39:13 -0500

    Taking a break.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// GET users, Idex Users 
 router.get('/', function(req, res, next) {
   res.render('users/index', {})
 })
+
+// 2. POST, Create User
+
+// PUT
+
+// DELETE
+
+
+// 3. Show Single User
+
+// 4. 
+
+// 5. 
 
 module.exports = router
